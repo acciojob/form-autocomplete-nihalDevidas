a46904cbc9d775435cc7d82adddd491dfbcf217b
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2024-02-05 23:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const formtag = document.getElementById("form1");
+const ip = document.getElementById("ip1");
+ window.onload = ()=>{
+	let mail = localStorage.getItem("email");
+	 if(mail){
+		 ip.value = JSON.parse(mail)
+	 }
+	 	
+} 
+
+  
+const formHandler = (e)=>{
+	e.preventDefault() 
+	localStorage.setItem("email",JSON.stringify(ip.value));
+}
+
+formtag.addEventListener("submit",formHandler);       
+       
